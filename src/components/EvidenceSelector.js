192897387c52f31b369ae5ae3724b08b37052d18
larsnaslund <26661952+larsnaslund@users.noreply.github.com>
2021-11-16 21:44:04 +0100

    Created evidence selector component

--- src/components/EvidenceSelector.js
+++ src/components/EvidenceSelector.js
@@ -1,24 +1,48 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react'
+import { Card, Form } from 'react-bootstrap'
+import { EVIDENCE } from '../assets/json/ghosts'
 
+//TODO REWRITE ONCE FUNCTIONAL
 export default function EvidenceSelector() {
+
+    const [evidence, setEvidence] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        let tempEvidence = [];
+
+        Object.entries(EVIDENCE).map(([key, value]) =>
+            // Checkbox state: 0 = unchecked, 1 = checked, 2 = strike-through
+            tempEvidence.push({ id: key, label: value, state: 0 })
+        )
+        setIsLoaded(true);
+        return setEvidence(tempEvidence);
+    }, [])
+
+
+    const onChangeCheckbox = (id) => {
+        const checkedEvidence = evidence.find(item => item.id === id);
+
+        if (checkedEvidence) {
+            // If checked state is at the upper limit (2), reset to 0, otherwise increment
+            checkedEvidence.state = (checkedEvidence.state === 2) ? 0 : checkedEvidence.state + 1;
+            setEvidence([...evidence]);
+        }
+    }
+
+
     return (
-        <div>
-
-        </div>
+        <Card>
+            <Card.Header>Available evidence</Card.Header>
+            {isLoaded && evidence.map(
+                (entry) => (
+                    <Form.Check type="checkbox" id={entry.id}>
+                        {/* TODO get the ID dynamically instead of calling with a lambda */}
+                        <Form.Check.Input checked={entry.state === 2 || entry.state === 1} onChange={() => onChangeCheckbox(entry.id)} type="checkbox" />
+                        <Form.Check.Label style={entry.state === 2 ? { textDecorationLine: 'line-through' } : {}}>{entry.label}</Form.Check.Label>
+                    </Form.Check>
+                )
+            )}
+        </Card >
     )
-}
-import React from 'react'
-import { Card } from 'react-bootstrap'
-import GhostEvidence from './GhostEvidence';
-
-export default function GhostGrid(props) {
-    return (<div className="ghost-grid">
-        {props.list.map((ghost) => (
-            <Card>
-                <Card.Header className="ghost-name">{ghost.name}</Card.Header>
-                <GhostEvidence list={ghost.evidence} />
-            </Card>
-        ))}
-    </div>)
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { GHOSTS } from '../assets/json/ghosts';
+import EvidenceSelector from '../components/EvidenceSelector';
 import GhostGrid from '../components/GhostGrid';
 
 export default function Home() {
     return (
-        <GhostGrid list={GHOSTS} />
+        <>
+            <EvidenceSelector />
+            <GhostGrid list={GHOSTS} />
+        </>
     )
 }
