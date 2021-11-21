ce687cfced0c5af5fd7b1fd06da6834db1bc9410
Lars Näslund <26661952+larsnaslund@users.noreply.github.com>
2021-11-21 16:52:06 +0100

    Update api.js

--- src/mocks/api.js
+++ src/mocks/api.js
@@ -1,6 +1,6 @@
 /**
  * Mocking an API by using Mock Service Worker
- * This will let us make proper API calls in the code, which we in here as there's no proper API developed yet.
+ * This will let us make proper API calls in the code, which we respond to in here as there's no proper API developed yet.
  * 
  * Things could be split into files: browser.js, handlers.js, etc. to simplify extension for other environments, but this app will just focus on the browser for now.
  */
@@ -11,9 +11,7 @@ import { setupWorker } from 'msw'
 import { GHOSTS, EVIDENCE } from './db.data';
 
 const handlers = [
-
     rest.get('/api/ghosts', (req, res, ctx) => res(ctx.json(GHOSTS))),
-
     rest.get('/api/ghost-evidence', (req, res, ctx) => res(ctx.json(EVIDENCE))),
 ];
 
