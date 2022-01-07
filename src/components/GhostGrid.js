746c61de4166c1fd80ddd4387f67de9b743baa71
larsnaslund <26661952+larsnaslund@users.noreply.github.com>
2022-01-07 12:33:04 +0100

    * Added sticky note styling to ghost cards

--- src/components/GhostGrid.js
+++ src/components/GhostGrid.js
@@ -7,9 +7,19 @@ export default function GhostGrid(props) {
 
     const { ghosts } = useGhostHelper();
 
+    // Generates a random slight tilt. Hopefully brings some extra life into the notes
+    function randomTilt() {
+        const min = -0.7;
+        const max = 0.7;
+        const tiltDegree = Math.random() * (max - min + max) + min;
+        return {
+            transform: 'rotate(' + tiltDegree + 'deg)'
+        };
+    }
+
     return (<div className="ghost-grid">
         {ghosts.map((ghost) => (
-            <Card key={ghost.name.split(' ').join('')}>
+            <Card key={ghost.name.split(' ').join('')} style={randomTilt()}>
                 <Card.Header className="ghost-name">{ghost.name}</Card.Header>
                 <GhostEvidence list={ghost.evidence} />
             </Card>
